b1f556caaf9645c8c6f8b28ff31b5c6e046decd1
guilherme <78052265+Guilherme275149@users.noreply.github.com>
2021-05-07 18:48:43 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const ownerNumber = ["557598868032\n5521971405324@s.whatsapp.net"]
+const ownerNumber = ["557598868032@s.whatsapp.net"]
 // mude somente o numero e não tire o @s.whatsapp.net
 
 const OriginalOwner = '557598868032' 
